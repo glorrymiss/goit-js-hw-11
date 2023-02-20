a7584de78cad68f8eb22d32b362abdb687a6e693
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2023-02-20 10:33:20 +0100

    add change

--- src/index.js
+++ src/index.js
@@ -1,134 +1,139 @@
-import Notiflix from 'notiflix';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-const axios = require('axios');
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+// import Notiflix from 'notiflix';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// const axios = require('axios/dist/browser/axios.cjs');
 
-const refs = {
-  form: document.querySelector('#search-form'),
-  container: document.querySelector('.gallery'),
-  btnLoad: document.querySelector('.load-more'),
-};
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
 
-refs.form.addEventListener('submit', onSearch);
-refs.btnLoad.addEventListener('click', loadMore);
+// let inputValue = '';
+// let page = 1;
+// let per_page = 200;
+// let gallery = new SimpleLightbox('.gallery a');
+// const URL = 'https://pixabay.com/api/';
+// const KEY = '33635231-9592dead0045fe81be9248485';
 
-let inputValue = '';
-let page = 1;
-let per_page = 200;
-let gallery = new SimpleLightbox('.gallery a');
-const URL = 'https://pixabay.com/api/';
-const KEY = '33635231-9592dead0045fe81be9248485';
+// async function fetchArticles(page = 1) {
+//   return await axios
+//     .get(
+//       `${URL}?key=${KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${per_page}`
+//     )
+//     .then(response => response.data);
+//   // if (!response.ok) {
+//   //   throw new Error(response.statusText);
+//   // }
+// }
 
-function fetchArticles(page = 1) {
-  return fetch(
-    `${URL}?key=${KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${per_page}`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    return response.json();
-  });
-}
+// const refs = {
+//   form: document.querySelector('#search-form'),
+//   container: document.querySelector('.gallery'),
+//   btnLoad: document.querySelector('.load-more'),
+// };
 
-function onSearch(event) {
-  event.preventDefault();
+// refs.form.addEventListener('submit', onSearch);
+// refs.btnLoad.addEventListener('click', loadMore);
 
-  inputValue = event.currentTarget.elements.searchQuery.value.trim();
-  console.log(inputValue);
-  if (inputValue === '') {
-    onUpdate();
-    return;
-  }
-  onUpdate();
-  fetchArticles()
-    .then(data => {
-      if (data.hits === [] || data.hits.length === 0) {
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-        return '';
-      } else {
-        page = 1;
-        createMarkup(data.hits);
-        refs.btnLoad.hidden = false;
-        Notiflix.Notify.info(`Hooray! We found ${data.totalHits} images.`);
-        gallery.refresh();
-        const { height: cardHeight } = document
-          .querySelector('.gallery')
-          .firstElementChild.getBoundingClientRect();
+// async function onSearch(event) {
+//   event.preventDefault();
 
-        window.scrollBy({
-          top: cardHeight * -100,
-          behavior: 'smooth',
-        });
-      }
-    })
+//   inputValue = event.currentTarget.elements.searchQuery.value.trim();
+//   console.log(inputValue);
+//   if (inputValue === '') {
+//     onUpdate();
+//     return;
+//   }
+//   onUpdate();
+//   try {
+//     const response = await fetchArticles();
+//     // .then(data => {
+//     if (response.hits === [] || response.hits.length === 0) {
+//       Notiflix.Notify.failure(
+//         'Sorry, there are no images matching your search query. Please try again.'
+//       );
+//       return '';
+//     } else {
+//       page = 1;
+//       createMarkup(response.hits);
+//       refs.btnLoad.hidden = false;
+//       Notiflix.Notify.info(`Hooray! We found ${response.totalHits} images.`);
+//       gallery.refresh();
+//       // const { height: cardHeight } = document
+//       //   .querySelector('.gallery')
+//       //   .firstElementChild.getBoundingClientRect();
 
-    .catch(error => console.log(error));
-}
+//       // window.scrollBy({
+//       //   top: cardHeight * -100,
+//       //   behavior: 'smooth',
+//       // });
+//     }
+//     // });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-function loadMore() {
-  page += 1;
+// async function loadMore() {
+//   page += 1;
 
-  fetchArticles(page).then(data => {
-    createMarkup(data.hits);
-    gallery.refresh();
+//   const response = await fetchArticles(page);
+//   // .then(data => {
+//   createMarkup(response.hits);
+//   gallery.refresh();
 
-    const pages = Math.ceil(data.totalHits / per_page);
+//   const pages = Math.ceil(response.totalHits / per_page);
 
-    if (page === pages) {
-      refs.btnLoad.hidden = true;
-      refs.btnLoad.style.display = 'none';
-      Notiflix.Notify.info(
-        "We're sorry, but you've reached the end of search results."
-      );
-    }
-  });
-}
+//   if (page === pages) {
+//     refs.btnLoad.hidden = true;
+//     refs.btnLoad.style.display = 'none';
+//     Notiflix.Notify.info(
+//       "We're sorry, but you've reached the end of search results."
+//     );
+//   }
+//   // });
+// }
 
-function createMarkup(arr) {
-  const markup = arr
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `
-      <a class='card-link' href="${largeImageURL}">
-      <div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" width="320" height ="280"/>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes:</b> ${likes}
-    </p>
-    <p class="info-item">
-      <b>Wiews:</b> ${views}
-    </p>
-    <p class="info-item">
-      <b>Comments:</b> ${comments}
-    </p>
-    <p class="info-item">
-      <b>Downloads: </b>${downloads}
-    </p>
-  </div>
-</div>
-</a>
-    `;
-      }
-    )
-    .join('');
+// function createMarkup(arr) {
+//   const markup = arr
+//     .map(
+//       ({
+//         webformatURL,
+//         largeImageURL,
+//         tags,
+//         likes,
+//         views,
+//         comments,
+//         downloads,
+//       }) => {
+//         return `
+//       <a class='card-link' href="${largeImageURL}">
+//       <div class="photo-card">
+//   <img src="${webformatURL}" alt="${tags}" loading="lazy" width="320" height ="280"/>
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes:</b> ${likes}
+//     </p>
+//     <p class="info-item">
+//       <b>Wiews:</b> ${views}
+//     </p>
+//     <p class="info-item">
+//       <b>Comments:</b> ${comments}
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads: </b>${downloads}
+//     </p>
+//   </div>
+// </div>
+// </a>
+//     `;
+//       }
+//     )
+//     .join('');
 
-  refs.container.insertAdjacentHTML('beforeend', markup);
-  //  add lightbox
-  gallery = new SimpleLightbox('.gallery a');
-}
+//   refs.container.insertAdjacentHTML('beforeend', markup);
+//   //  add lightbox
+//   gallery = new SimpleLightbox('.gallery a');
+// }
 
-function onUpdate() {
-  refs.container.innerHTML = '';
-}
+// function onUpdate() {
+//   refs.container.innerHTML = '';
+// }
+import './js/main';
